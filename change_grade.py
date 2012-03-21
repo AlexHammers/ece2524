7774d1389667c76e964b903fc419c26f807f4c75
Alex Hammers <ahamm92@vt.edu>
2012-03-21 16:32:19 -0400

    Change function to class

--- change_grade.py
+++ change_grade.py
@@ -2,19 +2,30 @@
 
 import re
 import fileinput
-from sys import stderr,stdout,argv,exit
 
-if len(argv) != 3:
-    stderr.write("Wrong number of arguments\n")
-    exit(1)
+def change_grade(searchStr, grade, data):
+    lines=[]
+    for line in fileinput.input(data):
+    	l = re.search(searchStr, line)
+    	if l:
+	    values = line.split(',')
+	    values[3] = grade
+	    lines.append(",".join(values))
+    	else:
+	    lines.append(line)
+    return (lines)
 
-for line in fileinput.input('-'):
-    l = re.search(argv[1], line)
-    if l:
-	values = line.split(',')
-	values[3] = argv[2]
-	print ",".join(values)
-    else:
-	print line
+if __name__=='__main__':
+    from sys import stderr,stdout,argv,exit
+    if len(argv) != 3:
+    	stderr.write("Wrong number of arguments\n")
+    	exit(1)
+    (lines) = change_grade(argv[1], argv[2], stdin)
+    for line in lines:
+	stderr.write(line)
+
+
+
+
 
 
